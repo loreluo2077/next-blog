6dcbacc13eee0a9deb1852ea18311aaa3ac8d58d
loreluo2077 <loroluo2077@gmail.com>
2025-05-01 01:19:28 +0800

    防止页面变大

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,9 @@ export const metadata: Metadata = {
     viewport: {
         width: 'device-width',
         initialScale: 1,
+        minimumScale: 1,
         maximumScale: 1,
+        userScalable: false
     },
 };
 
